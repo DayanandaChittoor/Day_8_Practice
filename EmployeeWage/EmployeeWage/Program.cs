b513a16e8e3197e250944298bf7286cc90cd6eab
DayanandaChittoor <seenu.daya5@gmail.com>
2021-08-20 13:47:13 +0530

    Added employee wage program

---
+++ EmployeeWage/EmployeeWage/Program.cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+namespace EmployeeWage
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            EmployeeWage Emp1 = new EmployeeWage();
+            Emp1.EmployeeDetails();
+            Emp1.EmployeeCheck();
+        }
+    }
+}
